c67b9cd0f2735dea5a14325de1002096a1c39717
Ebugo <gospelokpara@gmail.com>
2020-05-28 13:53:37 +0100

    worked on password recovery

--- src/controllers/paymentsController.js
+++ src/controllers/paymentsController.js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 /* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
 /* eslint-disable func-names */
--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -6,12 +6,11 @@ const jwt = require('jsonwebtoken');
 const { check, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const sgMail = require('@sendgrid/mail');
-const config = require('../config/auth.config');
 const User = require('../models/Users');
 const usersCollection = require('../db').db('paytax').collection('users');
 
 exports.home = (req, res) => {
-// return payer Id by email
+  // return payer Id by email
   res.json('testing api');
 };
 
@@ -109,7 +108,7 @@ exports.recovery = [
       });
     }
     // create recovery token
-    const token = jwt.sign({ id: user.id }, config.secret, {
+    const token = jwt.sign({ id: user.taxPayerId }, process.env.JWTSECRET, {
       expiresIn: 7200 // 2 hours
     });
 
@@ -125,13 +124,13 @@ exports.recovery = [
         
         We received a request to use PayTax APP Account Recovery to regain access to your account. To continue, click this link:
         
-        <a href="https://paytaxapp.com/${token}"> Start LastPass Account Recovery </a>
+        <a href="${process.env.RECOVERY}${token}"> Start LastPass Account Recovery </a>
         
         The link expires in 2 hours.
         
         If the link doesn't work, visit this site by copying this address to your browser:
         
-        https://paytaxapp.com/${token} </pre>
+        ${process.env.RECOVERY}${token} </pre>
       `
     };
     sgMail.send(msg);
@@ -139,6 +138,70 @@ exports.recovery = [
       status: true,
       data: 'Message sent'
     });
+  }
+];
+
+exports.recoverPassword = [
+  [
+    // password must be at least 8 chars long
+    check('password').isLength({ min: 8 }).withMessage('Password must be at least 8 chars long'),
+    check('confirmPassword').isLength({ min: 8 }).withMessage('Password confirmation must be at least 8 chars long')
+  ], async (req, res) => {
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ status: false, errors: errors.array() });
+    }
+
+    try {
+      const { password } = req.body;
+      const { confirmPassword } = req.body;
+      if (password !== confirmPassword) return res.status(422).json({ status: false, data: 'Passwords do not match' });
+      const salt = bcrypt.genSaltSync(10);
+      const passwordHash = bcrypt.hashSync(password, salt);
+      // get access token
+      const { token } = req.query;
+      if (!token) {
+        return res.status(403).json({
+          status: false,
+          data: 'Sorry, you must provide a valid token.'
+        });
+      }
+      req.apiUser = jwt.verify(token, process.env.JWTSECRET);
+      const user = await usersCollection.findOneAndUpdate({ taxPayerId: req.apiUser.id },
+        { $set: { password: passwordHash } });
+      if (!user) {
+        return res.status(400).json({
+          status: false,
+          data: 'User not found'
+        });
+      }
+      // send success mail
+      sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+      const msg = {
+        to: `${user.value.email}`,
+        from: 'contact@app.paytax.com',
+        subject: 'PayTax APP Password Reset',
+        text: 'Pay your tax anywhere with app.paytax.com',
+        html: `
+        <h1> PayTax account recovery request </h1>
+        Hello ${user.value.businessName}, <br/>
+        
+        <p> Your password has been reset successfully! </p>
+      `
+      };
+      sgMail.send(msg);
+
+      return res.status(200).json({
+        status: true,
+        data: 'Password reset was successful'
+      });
+    } catch (error) {
+      return res.status(400).json({
+        status: false,
+        data: `Sorry, an error occured - ${error}`
+      });
+    }
   }
 ];
 
